7c68930a580bae726ea7c173a51c0ab504761a1b
Yasin <yasinm289@gmail.com>
2022-07-22 12:36:16 +0430

    some works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from './logo.svg';
 import './App.css';
 import './index.css';
 
@@ -7,18 +6,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href='https://google.com' className='text-blue-400'>Click me</a>
         <p class="text-red-700">My Name is Yasin</p>
       </header>
     </div>
