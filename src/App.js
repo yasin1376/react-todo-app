8fdb6e68abe3d7d1f245a051992e974be51adbd1
Yasin <yasinm289@gmail.com>
2022-07-23 15:40:51 +0430

    some changes

--- src/App.js
+++ src/App.js
@@ -3,23 +3,35 @@ import './App.css';
 import './index.css';
 
 function App() {
+
+  function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>;
+  }
+  
+
+  function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+  }
+
+  function getGreeting(user) {
+    if (user) {
+      return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>;
+  }
+
+  const user = {
+    firstName: 'Yasin',
+    lastName: 'Mohammadi'
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-          <img class="block mx-auto h-24 w-24 rounded-full sm:mx-0 sm:shrink-0" src="https://thumbs.dreamstime.com/b/portrait-perfection-male-barber-care-mens-beauty-hair-beard-care-charismatic-macho-man-profile-look-guy-sexy-stylish-174493835.jpg" alt="Woman's Face" />
-          <div class="text-center space-y-2 sm:text-left">
-            <div class="space-y-0.5">
-              <p class="text-lg text-black font-semibold">
-                Erin Lindford
-              </p>
-              <p class="text-slate-500 font-medium">
-                Product Engineer
-              </p>
-            </div>
-            <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
-          </div>
-        </div>
+        <Welcome name="Yasin" />
+        <h1>
+          {getGreeting(user)}
+        </h1>
       </header>
     </div>
   );
