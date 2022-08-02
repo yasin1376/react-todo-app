edc02403b51f976d8b34f42ae4fd5341de70a996
Yasin <yasinm289@gmail.com>
2022-08-02 13:34:27 +0430

    working on todo list app

--- src/App.js
+++ src/App.js
@@ -4,33 +4,12 @@ import './index.css';
 
 function App() {
 
-  function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
-  }
-
-  function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-  }
-
-  function getGreeting(user) {
-    if (user) {
-      return <h1>Hello, {formatName(user)}!</h1>;
-    }
-    return <h1>Hello, Stranger.</h1>;
-  }
-
-  const user = {
-    firstName: 'Yasin',
-    lastName: 'Mohammadi'
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-        <Welcome name="Yasin" />
-        <h1>
-          {getGreeting(user)}
-        </h1>
+        <div>
+          Todo list App
+        </div>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,79 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import LoginButton from './LoginButton.js';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-class BlurExample extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { isOpen: false };
-    this.timeOutId = null;
-
-    this.onClickHandler = this.onClickHandler.bind(this);
-    this.onBlurHandler = this.onBlurHandler.bind(this);
-    this.onFocusHandler = this.onFocusHandler.bind(this);
-  }
-
-  onClickHandler() {
-    this.setState(currentState => ({
-      isOpen: !currentState.isOpen
-    }));
-  }
-
-  // We close the popover on the next tick by using setTimeout.
-  // This is necessary because we need to first check if
-  // another child of the element has received focus as
-  // the blur event fires prior to the new focus event.
-  onBlurHandler() {
-    this.timeOutId = setTimeout(() => {
-      this.setState({
-        isOpen: false
-      });
-    });
-  }
-
-  // If a child receives focus, do not close the popover.
-  onFocusHandler() {
-    clearTimeout(this.timeOutId);
-  }
-
-  render() {
-    // React assists us by bubbling the blur and
-    // focus events to the parent.
-    return (
-      <div onBlur={this.onBlurHandler}
-           onFocus={this.onFocusHandler}>
-        <button onClick={this.onClickHandler}
-                aria-haspopup="true"
-                aria-expanded={this.state.isOpen}>
-          Select an option
-        </button>
-        {this.state.isOpen && (
-          <ul>
-            <li>Option 1</li>
-            <li>Option 2</li>
-            <li>Option 3</li>
-          </ul>
-        )}
-      </div>
-    );
-  }
-}
-
-root.render(<BlurExample />);
-
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
