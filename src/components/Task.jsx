2529601dde49c3ebc32b42e90ce933ba30b1c1a5
Yasin <yasinm289@gmail.com>
2022-08-05 22:30:58 +0430

    added delete button

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 class Task extends Component {
     state = {  } 
     render() { 
-        return (<li>{this.props.task.name}</li>);
+        return (
+        <li>
+            {this.props.task.name}
+            <button className='rounded bg-red-700 text-sm p-1 ml-3' >Delete</button>
+        </li>
+        );
     }
 }
  
